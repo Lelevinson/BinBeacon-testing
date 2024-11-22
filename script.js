3f7dd526d49d13291a42e6a7c538eb7b0f9cb1d9
Levinson <levinsonyzu@gmail.com>
2024-11-22 14:16:34 +0800

    group marker var to geoJSON var

--- script.js
+++ script.js
@@ -77,23 +77,47 @@ var RecyclablePopUp = L.popup({
 var firstMarkerCoor = L.latLng(24.969748513353736, 121.26744248398677);
 var secondMarkerCoor = L.latLng(24.969338143170532, 121.26753221658362);
 
-// adding first marker
-var nonRec_1 = L.marker(firstMarkerCoor, {
-	draggable: true,
-	icon: nonRecyclableIcon,
-	alt: "non-recyclable", // if the image fail to show up
-	title: "first marker", // if hover cursor on marker, a browser tooltip will pop up
-	riseOnHover: true, // if 2 or more trash bin coordinate is close, then the one cursor hover will be on top
-})
-	.addTo(map)
-	.bindPopup(nonRecyclablePopUp);
+// creating geoJSON var for Recyclable and Non-Recyclable
+var geojson_Recyclable = {
+	type: "FeatureCollection",
+	features: [
+		{
+			type: "Feature",
+			properties: {},
+			geometry: {
+				coordinates: [121.2674297605979, 24.96978337951427],
+				type: "Point",
+			},
+		},
+		{
+			type: "Feature",
+			properties: {},
+			geometry: {
+				coordinates: [121.26805649859023, 24.96750871556256],
+				type: "Point",
+			},
+		},
+	],
+};
 
-// adding second marker
-var rec_1 = L.marker(secondMarkerCoor, {
-	draggable: true,
-	icon: recyclableIcon,
-	alt: "recyclable",
-	riseOnHover: true,
-})
-	.addTo(map)
-	.bindPopup(RecyclablePopUp);
+var geojson_NonRecyclable = {
+	type: "FeatureCollection",
+	features: [
+		{
+			type: "Feature",
+			properties: {},
+			geometry: {
+				coordinates: [121.26753586631207, 24.969182371314716],
+				type: "Point",
+			},
+		},
+		{
+			type: "Feature",
+			properties: {},
+			geometry: {
+				coordinates: [121.26621049477433, 24.969054169511196],
+				type: "Point",
+			},
+		},
+	],
+};
