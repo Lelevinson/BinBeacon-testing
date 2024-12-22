4927b291f6947b343a8f2bb4820a6d49a3674159
Levinson <levinsonyzu@gmail.com>
2024-12-22 16:40:25 +0800

    small formating

--- backendstuff/server.js
+++ backendstuff/server.js
@@ -1,17 +1,17 @@
 import express from "express";
-import cors from 'cors';
-import { gettingCoords } from './markergetter.js';
+import cors from "cors";
+import { gettingCoords } from "./markergetter.js";
 
 const app = express();
 const port = 3000;
 
 app.use(cors());
 
-app.get('/', async (req, res) => {
-  const coords = await gettingCoords();
-  res.json(coords);
+app.get("/", async (req, res) => {
+	const coords = await gettingCoords();
+	res.json(coords);
 });
 
 app.listen(port, () => {
-  console.log('running server on port:', port);
+	console.log("running server on port:", port);
 });
